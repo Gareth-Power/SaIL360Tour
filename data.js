cd6018b6f304e06e5e43e982232666727bd4d2c7
Gareth Power <41906466+Gareth-Power@users.noreply.github.com>
2023-10-06 11:25:25 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,431 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-high-fidelity-room",
+      "name": "High-Fidelity Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2640,
+      "initialViewParameters": {
+        "yaw": -2.0169451121457094,
+        "pitch": 0.378667734529067,
+        "fov": 1.4612486697297087
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21004849446092777,
+          "pitch": 0.17533205603892732,
+          "rotation": 0,
+          "target": "2-corridor-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-control-room",
+      "name": "Control Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2640,
+      "initialViewParameters": {
+        "yaw": 2.907765943596589,
+        "pitch": 0.2581015521830743,
+        "fov": 1.4612486697297087
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6535145491587002,
+          "pitch": 0.2260525676271996,
+          "rotation": 0,
+          "target": "3-corridor-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-corridor-4",
+      "name": "Corridor 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2640,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.694368564358892,
+          "pitch": 0.35250249473091877,
+          "rotation": 0,
+          "target": "0-high-fidelity-room"
+        },
+        {
+          "yaw": 0.7991487754922098,
+          "pitch": 0.32541895026045253,
+          "rotation": 0,
+          "target": "3-corridor-3"
+        },
+        {
+          "yaw": -2.1673020048264746,
+          "pitch": 0.372672682113933,
+          "rotation": 0,
+          "target": "8-seminar-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-corridor-3",
+      "name": "Corridor 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2640,
+      "initialViewParameters": {
+        "yaw": -2.8075339569196736,
+        "pitch": 0.3194933341004109,
+        "fov": 1.4612486697297087
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.39713394387192835,
+          "pitch": 0.3043083992776907,
+          "rotation": 0,
+          "target": "4-corridor-2"
+        },
+        {
+          "yaw": 2.8583156915396355,
+          "pitch": 0.2365428065959918,
+          "rotation": 0,
+          "target": "1-control-room"
+        },
+        {
+          "yaw": -2.804032031122947,
+          "pitch": 0.3304441798042621,
+          "rotation": 6.283185307179586,
+          "target": "2-corridor-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-corridor-2",
+      "name": "Corridor 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2640,
+      "initialViewParameters": {
+        "yaw": -0.9819714985092531,
+        "pitch": 0.10401406486262843,
+        "fov": 1.4612486697297087
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.277743533516553,
+          "pitch": 0.5550248571523753,
+          "rotation": 0,
+          "target": "5-corridor-1"
+        },
+        {
+          "yaw": -1.0784617561978358,
+          "pitch": 0.023605537761245188,
+          "rotation": 0,
+          "target": "3-corridor-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-corridor-1",
+      "name": "Corridor 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2640,
+      "initialViewParameters": {
+        "yaw": 1.6779958478184351,
+        "pitch": 0.32177328051000487,
+        "fov": 1.4612486697297087
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09044703319605496,
+          "pitch": 0.41558108028188556,
+          "rotation": 0,
+          "target": "6-entrance"
+        },
+        {
+          "yaw": 1.715091132452729,
+          "pitch": 0.39403602033177165,
+          "rotation": 0,
+          "target": "4-corridor-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-entrance",
+      "name": "Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2640,
+      "initialViewParameters": {
+        "yaw": -3.0004118960268897,
+        "pitch": 0.22228950994156804,
+        "fov": 1.4612486697297087
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1185245206047156,
+          "pitch": 0.2496216802848501,
+          "rotation": 6.283185307179586,
+          "target": "5-corridor-1"
+        },
+        {
+          "yaw": 2.482398876711672,
+          "pitch": 0.5546436778495334,
+          "rotation": 0,
+          "target": "7-ward"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-ward",
+      "name": "Ward",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2640,
+      "initialViewParameters": {
+        "yaw": 0.5641270298931431,
+        "pitch": 0.19106972223007013,
+        "fov": 1.4612486697297087
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6839482087955506,
+          "pitch": 0.31714501543527795,
+          "rotation": 0,
+          "target": "6-entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-seminar-room",
+      "name": "Seminar Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2640,
+      "initialViewParameters": {
+        "yaw": -0.6298338917940001,
+        "pitch": 0.20332498381291586,
+        "fov": 1.4612486697297087
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2496749815025794,
+          "pitch": 0.21704071314875684,
+          "rotation": 0,
+          "target": "2-corridor-4"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "SaIL 360 Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
